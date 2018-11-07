bc06e32b68f707b57f6b80662e36cc4184d3a102
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-11-06 19:44:00 -0700

    Remove Options

--- ScrapeWebsite/index.js
+++ ScrapeWebsite/index.js
@@ -1,13 +1,8 @@
-const { optionsRequest } = require("../src/OptionsRequest");
 const { postRequest } = require("../src/PostRequest");
 
 module.exports = async function(context, req) {
   try {
-    if (req.method == "OPTIONS") {
-      context.res = optionsRequest(req);
-    } else {
-      context.res = await postRequest(req);
-    }
+    context.res = await postRequest(req);
   } catch (error) {
     context.res = {
       status: 500,
