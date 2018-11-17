4cbcb5fa4f0c9540dfa076d43035d72808863017
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-11-16 20:39:45 -0700

    Does temp authorization now!

--- ScrapeWebsite/index.js
+++ ScrapeWebsite/index.js
@@ -3,13 +3,22 @@ const { validate } = require("../src/Auth");
 
 module.exports = async function(context, req) {
   try {
-    const valid = validate(req);
+    if (!(await validate(req))) {
+      throw "Unauthorized";
+    }
 
     context.res = await postRequest(req);
   } catch (error) {
-    context.res = {
-      status: 500,
-      body: error
-    };
+    if (error === "Unauthorized") {
+      context.res = {
+        status: 401,
+        body: { error: "Unauthorized" }
+      };
+    } else {
+      context.res = {
+        status: 500,
+        body: error
+      };
+    }
   }
 };
--- src/Auth.js
+++ src/Auth.js
@@ -1,6 +1,60 @@
 const aes256 = require("aes256");
+const azure = require("azure-storage");
+const tableService = azure.createTableService();
+const tempKeysTable = "tempkeys";
+const entGen = azure.TableUtilities.entityGenerator;
 
-function validate(req) {
+function createTableIfNotExists() {
+  tableService.createTableIfNotExists(tempKeysTable, function(error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
+function makeid() {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function insertTempKeyLog(decrypted) {
+  const entity = {
+    PartitionKey: entGen.String(decrypted),
+    RowKey: entGen.String(makeid())
+  };
+
+  tableService.insertEntity(tempKeysTable, entity, function(error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
+async function checkTempKeyLimit(tempKey) {
+  return new Promise(resolve => {
+    createTableIfNotExists();
+
+    let query = new azure.TableQuery()
+      .select(["partitionKey"])
+      .top(10)
+      .where("PartitionKey eq ?", tempKey);
+
+    tableService.queryEntities(tempKeysTable, query, null, function(
+      error,
+      result
+    ) {
+      resolve(result.entries.length >= 10 ? false : true);
+    });
+  });
+}
+
+async function validate(req) {
   const tempKey = req.headers["x-temp-key"];
   if (!tempKey) {
     throw "Unauthorized";
@@ -11,11 +65,12 @@ function validate(req) {
     req.headers["x-temp-key"]
   );
 
-  if (!parseInt(decrypted)) {
+  if (!parseInt(decrypted) || !(await checkTempKeyLimit(decrypted))) {
     return false;
+  } else {
+    insertTempKeyLog(decrypted);
+    return true;
   }
-
-  return true;
 }
 
 module.exports.validate = validate;
