c75c6ffff4fb21e1f2d1defe2816ad5322d332b3
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-11-16 20:00:07 -0700

    Basic temp auth

--- ScrapeWebsite/index.js
+++ ScrapeWebsite/index.js
@@ -1,7 +1,22 @@
 const { postRequest } = require("../src/PostRequest");
+const aes256 = require("aes256");
 
 module.exports = async function(context, req) {
   try {
+    const tempKey = req.headers["x-temp-key"];
+    if (!tempKey) {
+      throw "Unauthorized";
+    }
+
+    const decrypted = aes256.decrypt(
+      "LHGebvoencfCTprzeNwyGiT2ejJr8dSEAPTCK2QRjhbOkHl1PhfDLXGvlwyItBWL",
+      req.headers["x-temp-key"]
+    );
+
+    if (!parseInt(decrypted)) {
+      throw "Unauthorized";
+    }
+
     context.res = await postRequest(req);
   } catch (error) {
     context.res = {
